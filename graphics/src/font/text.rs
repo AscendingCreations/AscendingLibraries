4a4e363ee90be59381b25f04355b4bdcf019b6cc
genusistimelord <genusistimelord@outlook.com>
2025-04-11 15:39:36 -0400

    Updated comment to Fix incorrect documentation

--- graphics/src/font/text.rs
+++ graphics/src/font/text.rs
@@ -660,7 +660,6 @@ impl Text {
 
     /// Allows measuring the String character's Glyph and returning a Vec of their Sizes per character.
     /// This will not create any buffers in the rendering system.
-    /// This Returns the Exact Width and Height of the character. This will not Return the Line Height for Height.
     ///
     pub fn measure_glyphs(
         font_system: &mut FontSystem,
